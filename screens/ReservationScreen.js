93d618c03213681e12ca4b769e29a0dc88f196d7
Sharon Lewis <sflewis827@yahoo.com>
2022-11-12 18:00:18 -0500

    Modal

--- screens/ReservationScreen.js
+++ screens/ReservationScreen.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { 
   Button, 
+  Modal,
   ScrollView, 
   StyleSheet, 
   Switch, 
@@ -15,6 +16,7 @@ const ReservationScreen = () => {
   const [hikeIn, setHikeIn] = useState(false);
   const [date, setDate] = useState(new Date());
   const [showCalendar, setShowCalendar] = useState(false);
+  const [showModal, setShowModal] = useState(false);
 
   const onDateChange = (event, selectedDate) => {
     const currentDate = selectedDate || date;
@@ -26,12 +28,15 @@ const ReservationScreen = () => {
     console.log('campers:', campers);
     console.log('hikeIn:', hikeIn);
     console.log('date:', date);
+    setShowModal(!showModal);
+  };
 
+  const resetForm = () => {
     setCampers(1);
     setHikeIn(false);
     setDate(new Date());
     setShowCalendar(false);
-  }
+  };
 
   return (
     <ScrollView>
@@ -85,6 +90,35 @@ const ReservationScreen = () => {
           accessibilityLabel='Tap me to search for available campsites to reserve'
         />
       </View>
+      <Modal
+        animationType='slide'
+        transparent={false}
+        visible={showModal}
+        onRequstClose={() => setShowModal(!showModal)}
+      >
+        <View style={styles.modal}>
+          <Text style={styles.modalTitle}>
+            Search Campsite Reservations
+          </Text>
+          <Text style={styles.modalText}>
+            Number of Campers: {campers}
+          </Text>
+          <Text style={styles.modalText}>
+            Hike-In? {hikeIn ? 'Yes' : 'No'}
+          </Text>
+          <Text style={styles.modalText}>
+            Date: {date.toLocaleDateString('en-US')}
+          </Text>
+          <Button
+            onPress={() => {
+              setShowModal(!showModal);
+              resetForm();
+            }}
+            color='#5637DD'
+            title='Close'
+          />
+        </View>
+      </Modal>
     </ScrollView>
   )
 };
@@ -103,6 +137,22 @@ const styles = StyleSheet.create({
   },
   formItem: {
     flex: 1
+  },
+  modal: {
+    justifyContent: 'center',
+    margin: 20
+  },
+  modalTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    backgroundColor: '#5637DD',
+    textAlign: 'center',
+    color: '#fff',
+    marginBottom: 20
+  },
+  modalText: {
+    fontSize: 18,
+    margin: 10
   }
 });
 
