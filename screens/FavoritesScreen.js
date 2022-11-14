0e087b40f39f0849c984e478c2fc6ffd8c474a4e
Sharon Lewis <sflewis827@yahoo.com>
2022-11-13 19:46:43 -0500

    Alert

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,5 +1,6 @@
 import { useDispatch, useSelector } from 'react-redux';
 import {
+  Alert,
   FlatList,
   StyleSheet,
   Text,
@@ -26,7 +27,21 @@ const FavoritesScreen = ({ navigation }) => {
         <View style={styles.deleteView}>
           <TouchableOpacity
             style={styles.deleteTouchable}
-            onPress={() => dispatch(toggleFavorite(campsite.id))}
+            onPress={() => Alert.alert('Delete Favorite?',
+              'Are you sure you wish to delete the favorite campsite ' + campsite.name + '?',
+              [
+                {
+                  text: 'Cancel',
+                  onPress: () => console.log(campsite.name + ' Not Deleted'),
+                  style: 'cancel'
+                },
+                {
+                  text: 'OK',
+                  onPress: () => dispatch(toggleFavorite(campsite.id))
+                }
+              ],
+              { cancelable: false }
+            )}
           >
             <Text style={styles.deleteText}>Delete</Text>
           </TouchableOpacity>
