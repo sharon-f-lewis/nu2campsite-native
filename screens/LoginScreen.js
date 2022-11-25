00aafcc03b060d3459106996ca1dab9a6ceff78f
Sharon Lewis <sflewis827@yahoo.com>
2022-11-25 14:03:20 -0500

    Register Form

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -19,12 +19,12 @@ const LoginTab = ({ navigation }) => {
         'userinfo',
         JSON.stringify({
           username,
-          password
+          password,
         })
       ).catch((error) => console.log('Could not save user info', error));
     } else {
-      SecureStore.deleteItemAsync('userinfo')
-        .catch((error) => console.log('Could not delete user info ', error)
+      SecureStore.deleteItemAsync('userinfo').catch((error) =>
+        console.log('Could not delete user info ', error)
       );
     }
   };
@@ -59,7 +59,7 @@ const LoginTab = ({ navigation }) => {
         containerStyle={styles.formInput}
         leftIconContainerStyle={styles.formIcon}
       />
-      <CheckBox 
+      <CheckBox
         title='Remember Me'
         center
         checked={remember}
@@ -99,11 +99,114 @@ const LoginTab = ({ navigation }) => {
         />
       </View>
     </View>
-  )
+  );
 };
 
 const RegisterTab = () => {
-  return <ScrollView></ScrollView>
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [remember, setRemember] = useState(false);
+
+  const handleRegister = () => {
+    const userInfo = {
+      username,
+      password,
+      firstName,
+      lastName,
+      email,
+      remember,
+    };
+
+    console.log(JSON.stringify(userInfo));
+
+    if (remember) {
+      SecureStore.setItemAsync(
+        'userinfo',
+        JSON.stringify({
+          username,
+          password,
+        })
+      ).catch((error) => console.log('Could not save user info', error));
+    } else {
+      SecureStore.deleteItemAsync('userinfo').catch((error) =>
+        console.log('Could not delete user info ', error)
+      );
+    }
+  };
+
+  return (
+    <ScrollView>
+      <View style={styles.container}>
+        <Input
+          placeholder='Username'
+          leftIcon={{ type: 'font-awesome', name: 'user-o' }}
+          onChangeText={(text) => setUsername(text)}
+          value={username}
+          containerStyle={styles.formInput}
+          leftIconContainerStyle={styles.formIcon}
+        />
+        <Input
+          placeholder='Password'
+          leftIcon={{ type: 'font-awesome', name: 'key' }}
+          onChangeText={(text) => setPassword(text)}
+          value={password}
+          containerStyle={styles.formInput}
+          leftIconContainerStyle={styles.formIcon}
+        />
+        <Input
+          placeholder='First Name'
+          leftIcon={{ type: 'font-awesome', name: 'user-o' }}
+          onChangeText={(text) => setFirstName(text)}
+          value={firstName}
+          containerStyle={styles.formInput}
+          leftIconContainerStyle={styles.formIcon}
+        />
+        <Input
+          placeholder='Last Name'
+          leftIcon={{ type: 'font-awesome', name: 'user-o' }}
+          onChangeText={(text) => setLastName(text)}
+          value={lastName}
+          containerStyle={styles.formInput}
+          leftIconContainerStyle={styles.formIcon}
+        />
+        <Input
+          placeholder='Email'
+          leftIcon={{ type: 'font-awesome', name: 'envelope-o' }}
+          onChangeText={(text) => setEmail(text)}
+          value={email}
+          containerStyle={styles.formInput}
+          leftIconContainerStyle={styles.formIcon}
+        />
+        <CheckBox
+          title='Remember Me'
+          center
+          checked={remember}
+          onPress={() => setRemember(!remember)}
+          containerStyle={styles.formCheckbox}
+        />
+        <View style={styles.formButton}>
+          <Button
+            onPress={() => handleRegister()}
+            title='Register'
+            color='#5637DD'
+            icon={
+              <Icon
+                name='user-plus'
+                type='font-awesome'
+                color='#fff'
+                iconStyle={{ marginRight: 10 }}
+              />
+            }
+            buttonStyle={{ backgroundColor: '#5637DD' }}
+          />
+        </View>
+
+      </View>
+    </ScrollView>
+  );
 };
 
 const Tab = createBottomTabNavigator();
@@ -114,7 +217,7 @@ const LoginScreen = () => {
     inactiveBackgroundColor: '#CEC8FF',
     activeTintColor: '#fff',
     inactiveTintColor: '#808080',
-    labelStyle: { fontSize: 16 }
+    labelStyle: { fontSize: 16 },
   };
 
   return (
@@ -125,13 +228,9 @@ const LoginScreen = () => {
         options={{
           tabBarIcon: (props) => {
             return (
-              <Icon
-                name='sign-in'
-                type='font-awesome'
-                color={props.color}
-              />
+              <Icon name='sign-in' type='font-awesome' color={props.color} />
             );
-          }
+          },
         }}
       />
       <Tab.Screen
@@ -140,13 +239,9 @@ const LoginScreen = () => {
         options={{
           tabBarIcon: (props) => {
             return (
-              <Icon
-                name='user-plus'
-                type='font-awesome'
-                color={props.color}
-              />
+              <Icon name='user-plus' type='font-awesome' color={props.color} />
             );
-          }
+          },
         }}
       />
     </Tab.Navigator>
@@ -156,20 +251,24 @@ const LoginScreen = () => {
 const styles = StyleSheet.create({
   container: {
     justifyContent: 'center',
-    margin: 20
+    margin: 10
   },
   formIcon: {
     marginRight: 10
   },
   formInput: {
-    padding: 10
+    padding: 8,
+    height: 60
   },
   formCheckbox: {
-    margin: 10,
+    margin: 8,
     backgroundColor: null
   },
   formButton: {
-    margin: 40
-  }
-})
+    margin: 20,
+    marginRight: 40,
+    marginLeft: 40
+  },
+});
+
 export default LoginScreen;
